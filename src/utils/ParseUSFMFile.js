47e1555e440416fd44d514db42345d11f27c84b7
neetuy <neetuy1455@gmail.com>
2018-09-26 14:20:00 +0530

    added verses to chapters

--- src/utils/ParseUSFMFile.js
+++ src/utils/ParseUSFMFile.js
@@ -1,125 +1,116 @@
-import RNFS from 'react-native-fs'
-import React, {Component} from 'react';
-import {Text,Platform,ScrollView} from 'react-native'
+// import BookModel from '../models/BookModel'
+// import ChapterModel from '../models/ChapterModel'
+// import VerseComponentsModel from '../models/VerseComponentsModel'
+// import DbQueries from './dbQueries'
+// import id_name_map from '../assets/mappings.json'
+// const Constants = require('./constants')
+import React, {Component} from  'react'
+import {View,Text} from 'react-native'
 
+var RNFS = require('react-native-fs');
 
-export default class ParseData extends Component{
+export default class USFMParser extends Component{
     constructor(){
         super()
-        this.state={
-            // book:{
-            //     bookId:'',
-            //     chapters:{
-            //         chapNum:[
-            //             {
-            //                 verseNum:[],
-            //                 verseText:[]
-            //             }
-            //         ],
-                    
-            //     }
-            // }
-        }
+        this.bookId = null;
+        this.book = []
+        this.chapterList = [];
+        this.verseList = [];
+       
     }
-   
+
     async parseFile(){
-        chapterNumber= []
-        bookId =''
-        verseData= []
-        allMarker= []
         try {
             var content = await RNFS.readFileAssets('01-GEN.usfm')
-            this.setState({res:content})
-            var line = content.split('\n')
-            // console.log(line)
-            if(line.length != 0){
-                for(i=0; i<=line.length-1; i++){
-                 var marker = line[i].substr(0,line[i].indexOf(' '))
-                    allMarker.push(marker)         
-                    if(marker == '\\id'){
-                        var bookId =  line[i].substr(line[i].indexOf(' ')+1)//
-                        // console.log("book id "+bookId)
-                    }
-                    if(marker == '\\c'){
-                         var chapter =  line[i].substr(line[i].indexOf(' ')+1)//
-                         // console.log("chapter "+chapter)
-                            var chapterNum = line[i].substr(line[i].indexOf(' ')+1)
-                            chapterNumber.push(chapterNum)
-                    }
-                    if(marker == '\\v'){
-                        var text =  line[i].substr(line[i].indexOf(' ')+1)//
-                            verseData.push(text)
-                    }
-                   
-                    // console.log("chapter number "+chapterNumber)
+            this.parseFileContents(content)
+        }
+        catch(error){
+            console.log("error "+error)
+        }
+    }
+    parseFileContents(result) {
+        try {
+            var lines = result.split('\n');
+            for(var i = 0; i < lines.length; i++) {
+                //code here using lines[i] which will give you each line
+                if (!this.processLine(lines[i])) {
+                    return false;
                 }
-                this.addChapter(chapterNum);
-                this.addBookId(bookId)
-                this.addVerseData(verseData,chapterNum,allMarker)
-                this.getMarker(allMarker)
             }
+        } catch(exception) {
+            console.log("error in parsing file : " + exception)
+        }
+    }
 
-          }catch (e) {
-            alert("" + e);
-          }
-    }
-    getMarker(markers){
-            console.log("markers")
-    }
-    addBookId(id){
-        // console.log("id "+id)
-    }
-    addChapter(chapterNum){
-        // console.log("chapter"+chapterNum)
-    }
-    addVerseData(verseData,chapter,markers){
-        var verse = []
-        var verseArray = []
-        console.log("all marker "+JSON.stringify(markers))
-        for(i=0;i<=markers.length-1;i++){
-                if(markers[i]=='\\c'){
-                    console.log("matched marker c"+markers[i])
-                    if(markers[i+1] == '\\v'){
-                console.log("matched marker v "+markers[i+1].length)
+    processLine(line) {
+        var splitString = line.split(/\s+/);
+        if (splitString.length == 0) {
+            return true;
+        }
+        switch (splitString[0]){
+            case "\\id": {
+                if (!this.addBook(splitString[1])) {
+                    return false;
                 }
+                break;
+            }
+            case "\\c": {
+                this.addChapter(splitString[1]);
+                break;
+            }
+         
+            case "\\v": {
+                this.addVerse(splitString);
+                break;
+            }
+            case "":{
+                break;
+            }
+            default: {
+                break
+                
             }
         }
-        // console.log("versedaata"+verseData)
-        // for(i=0; i<=verseData.length-1; i++){
-        //     const verseNumber = verseData[i].substr(0,verseData[i].indexOf(' ')+1)
-        //     const verseText = verseData[i].substr(verseData[i].indexOf(' ')+1)
-        //     verse.push({
-        //         verseNumber:verseNumber,
-        //         verseText:verseText
-        //     })
-        // }
-        
-        // if(chapter.length >0){
-        //        for(i=0;i<=chapter.length-1;){
-        //            for(j=0;j<=verse[j].verseNumber.length-1;j++){
-        //                 console.log("verse number in loop "+verse[j].verseNumber)
-        //                 verseArray.push(verse[i].verseNumber)
-
-        //                 console.log("i "+i+ " j "+j)
-        //                 // i++
-        //                 // break;
-        //             console.log("verse number "+JSON.stringify(verseArray.len))
-        //            }
-
-        //        }
-        // }
+        return true;
     }
+
+
+    addBook(value) {
+        this.bookId = value.toString().trim();
+        return true;
+    }
+
+    addChapter(num) {
+       var chapter = {chapterNumber:num,verseList:[]} 
+       this.chapterList.push(chapter)
+
+    }
+    addVerse(splitString){
+        var verseNum = splitString[1];
+        // console.log("chapter list "+JSON.stringify(this.chapterList))
+        text = [];
+        for (var i=2; i<splitString.length; i++) {
+            text.push(splitString[i]);
+        }
+        var verse = {verseNumber:verseNum,verseText:text}
+        this.verseList.push(verse)
+        this.chapterList[this.chapterList.length - 1].verseList.push(this.verseList);
+        console.log("chapters in verse  "+JSON.stringify(this.chapterList))
+
+
+        // this.book.push(this.chapterList)
+        this.chapterList = []
+        this.verseList = []
+            // console.log("add verse "+JSON.stringify(this.book))
+    }
+    
     componentDidMount(){
-            this.parseFile()
+        this.parseFile()
     }
-
+   
     render(){
-        // console.log("verse data length number "+this.state.verse.length)
         return(
-        <ScrollView>
-        <Text>{JSON.stringify(this.state.verseData)}</Text>
-        </ScrollView>
+            <Text>doing parsing</Text>
         )
     }
 }
-
