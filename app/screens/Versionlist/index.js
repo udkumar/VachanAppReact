04f1b88802ecc0ba43967e8d61d856c28e7ac6cf
neetuy <neetuy1455@gmail.com>
2019-04-22 12:55:43 +0530

    showed list if language and version from api

--- app/screens/Versionlist/index.js
+++ app/screens/Versionlist/index.js
@@ -4,9 +4,7 @@ import { View } from 'native-base';
 
 export default class Versionlist extends Component {
     constructor(props) {
- 
         super(props);
-      
         console.log("propsvalues........."+JSON.stringify(this.props.navigation.state.params))
         this.state = {
           langaugeProp:this.props.navigation.state.params ? this.props.navigation.state.params.languages.language : null,
@@ -14,18 +12,16 @@ export default class Versionlist extends Component {
         }
     }
     _onLongPressButton =()=> {
-     
       this.props.screenProps.Navigatehome()
     }
     componentDidMount() {
-   
       return fetch('https://stagingapi.autographamt.com/app/versions')
         .then((response) => response.json())
         .then((responseJson) => {
           var data = []
           for(var key in responseJson.bible){
               if(key == this.state.langaugeProp){
-                console.log("version response key"+JSON.stringify(responseJson.bible[key]))
+                console.log("VERSION RESPONSE KEY"+JSON.stringify(responseJson.bible[key]))
                 data.push(responseJson.bible[key])
               }
           }
@@ -33,24 +29,23 @@ export default class Versionlist extends Component {
             isLoading: false,
             versionData:data
             })
-        
         })
         .catch((error) => {
           console.error(error);
         });
-        
     }
-   
+  
   render() {
     console.log("version data "+JSON.stringify(Object.keys(this.state.versionData)))
     return (
       <View style={{flex:1}}>
       {
          this.state.versionData.length > 0 ?  
-          Object.keys(this.state.versionData).map((item,i) =>(
-            <Text key={i}>{this.state.versionData[item].version_id}</Text>
-          ))
-         
+         this.state.versionData.map((item)=>{
+          return Object.keys(item).map((value,i) =>
+          <Text key={i}>{item[value].version_id}</Text>
+        )
+         })
          : null 
        } 
      </View>
