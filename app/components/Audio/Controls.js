3d09c2ccdda17baec19354df93612a0209dd901d
neetuy <neetuy1455@gmail.com>
2020-08-20 23:21:22 +0530

    made fixed size of audio icons should not change with text size from settings

--- app/components/Audio/Controls.js
+++ app/components/Audio/Controls.js
@@ -16,21 +16,21 @@ const Controls = ({
     <View style={styles.controlerContainer}>
       <View style={{ width: 20 }} />
       <TouchableOpacity onPress={onBack}>
-        <Icon onPress={onBack} style={styles.iconStyle} name="fast-rewind" size={24} />
+        <Icon onPress={onBack} style={styles.iconStyle} name="fast-rewind" size={32} />
       </TouchableOpacity>
       <View style={{ width: 10 }} />
       {!paused ?
         <View style={styles.playButton}>
-          <Icon onPress={onPressPause} name="pause" size={24} style={[styles.iconStyle, forwardDisabled && { opacity: 0.3 }]} />
+          <Icon onPress={onPressPause} name="pause" size={32} style={[styles.iconStyle, forwardDisabled && { opacity: 0.3 }]} />
         </View>
         :
         <View style={styles.playButton}>
-          <Icon onPress={onPressPlay} style={styles.iconStyle} name="play-arrow" size={24} />
+          <Icon onPress={onPressPlay} style={styles.iconStyle} name="play-arrow" size={32} />
         </View>
       }
       <View style={{ width: 10 }} />
       <TouchableOpacity onPress={onForward}>
-        <Icon style={styles.iconStyle} name="fast-forward" size={24} />
+        <Icon style={styles.iconStyle} name="fast-forward" size={32} />
       </TouchableOpacity>
       <View style={{ width: 20 }} />
     </View>
